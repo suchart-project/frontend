f92726113b3f0d92449e18bf20b8c7de7356b5cb
remove158 <remove158>
2021-11-21 12:18:29 +0700

    ✅ add : api connected to db

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introductio
-const DB = import("../../config/mongo");
-export default function handler(req, res) {
-	res.status(200).json({ name: "John Doe" });
+
+import { connectToDatabase } from "../../config/mongo";
+
+export default async function handler(req, res) {
+	const { db } = await connectToDatabase();
+	db.collection("movies").insertOne({ title: "test" });
+	const movies = await db.collection("movies").find({}).limit(20).toArray();
+	res.status(200).json({ name: "John Doe", movies });
 }
