7f1cc3711d1f2c12abab763c4ab0708ab37faa89
remove158 <remove158>
2021-11-21 12:16:58 +0700

    💄 style : layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Layout({ children }) {
 	return (
-		<div className="min-h-screen  bg-gray-50">
+		<div className="min-h-screen  relative bg-gray-50">
 			<div className="flex container mx-auto  gap-6 flex-col p-6">
 				{children}
 			</div>
