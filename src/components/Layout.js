ba3a0b9b9ef89b0da163e85f45efc1bbaf8428d5
Marvin <nopparut.bit@hotmail.com>
2021-11-22 00:52:16 +0700

    fix : fix layout of editProfile page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import React,{ useState } from "react";
 export default function Layout({ children,padding }) {
 	return (
 		<div className="min-h-screen relative bg-gray-50">
-			<div className={`flex container mx-auto  gap-6 flex-col p-${padding || '6'}`}>
+			<div className="flex container mx-auto  gap-6 flex-col p-6">
 				{children}
 			</div>
 		</div>
