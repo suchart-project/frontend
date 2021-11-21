2139929602deb7cb33939e840571da208afd0d02
remove158 <remove158@gmail.com>
2021-11-22 02:44:22 +0700

    ✅ add : get layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,14 @@
 import "../styles/globals.css";
 import Head from "next/head";
 function MyApp({ Component, pageProps }) {
+	const getLayout = Component.getLayout || ((page) => page);
 	return (
 		<>
 			<Head>
 				<title>Med My Day</title>
 				<meta name="theme-color" content="#6366f1" />
 			</Head>
-			<Component {...pageProps} />
+			{getLayout(<Component {...pageProps} />)}
 		</>
 	);
 }
