52a4f34b449b5aab0aa06b8fa72dba5c89a6c484
Marvin <nopparut.bit@hotmail.com>
2021-11-22 18:28:03 +0700

    feat : feat search bar,not yet

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -7,11 +7,11 @@ export default async function handler(req, res) {
 	const { connection: sqlConnection } = await sql();
 
 	if (req.method === "GET") {
-		// Usecase: patient search for physician
-		// TODO : use a lot of queries to get physician that relate to search text
-		const { Username } = req.query;
-		if (!Username) {
-			const [result] = await sqlConnection.execute(`
+        // Usecase: patient search for physician
+        // TODO : use a lot of queries to get physician that relate to search text
+        let { SearchStr, OnlineFlag, DistanceFlag } = req.query;
+        if (!SearchStr) {
+            const [result] = await sqlConnection.execute(`
                 SELECT d.Username, c.Firstname, c.Lastname, c.Coordinate, c.Img_path, cn.Clinic_name, d.Total_patients, d.Avg_rating, d.Price_min, d.Price_max, d.Price_avg, s.Name
                 FROM DOCTOR d
                 NATURAL JOIN CUSTOMER c
@@ -21,23 +21,19 @@ export default async function handler(req, res) {
                 WHERE c.Type = 'DOCTOR'
                 ORDER BY Avg_rating DESC;
             `);
-			return res.status(200).json(result);
-		}
-		const [users] = await sqlConnection.execute(
-			`
-                SELECT d.Username, c.Firstname, c.Lastname, c.Coordinate, c.Img_path, cn.Clinic_name, d.Total_patients, d.Avg_rating, d.Price_min, d.Price_max, d.Price_avg, s.Name
-                FROM DOCTOR d
-                NATURAL JOIN CUSTOMER c
-                JOIN CLINIC cn ON d.Clinic_id = cn.Clinic_id
-                JOIN DOCTOR_SPECIALTY ds ON d.Username = ds.Username
-                JOIN SPECIALTY s ON ds.Specialty_id = s.Specialty_id
-                WHERE c.Type = 'DOCTOR' and c.Username = ?
-                ORDER BY Avg_rating DESC;`,
-			[Username]
-		);
-		// if (!users) return res.status(404).json({ error: "No users found" });
-		return res.status(200).json(users);
-	}
+            return res.status(200).json(result);
+        }
+        OnlineFlag = (OnlineFlag === 'true');
+        DistanceFlag = (DistanceFlag === 'true');
+        const [users] = await sqlConnection.execute(
+            `
+                CALL SearchDoctorsIncludeSpecialties(?, current_timestamp, ?, ?, ?, ?);
+            `,
+            [SearchStr, 100.509349, 13.782778, OnlineFlag, DistanceFlag]
+        );
+        // if (!users) return res.status(404).json({ error: "No users found" });
+        return res.status(200).json(users);
+    }
 	// if (req.method === "POST") {
 	// 	const { name, email, password } = req.body;
 	// 	if (!name || !email || !password)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,41 +2,95 @@ import Layout from "../components/Layout";
 import Navbar from "../components/Navbar";
 import SearchPerson from "../components/SearchPerson";
 import SearchTextWithFilter from "../components/SearchTextWithFilter";
-import { useState } from "react";
-import useSWR from "swr";
-import Spinner from "../components/Spinner";
-import { fetcher } from "../config/config";
-export default function Home({}) {
-	const [filterOption, setFilterOption] = useState({});
-	const { data, error } = useSWR("/api/user", fetcher);
-	if (error) return "Something went wrong";
-	if (!data) return <Spinner />;
+import { useState, useCallback, useEffect } from "react";
 
-	return (
-		<>
-			<input
-				className="textbox"
-				placeholder="ค้นหาแพทย์ โดย ชื่อ / อาการ / คลินิค"
-			/>
-			<SearchTextWithFilter
-				text='All result for "สุชาติ"'
-				setFilter={setFilterOption}
-			/>
-			{data.map((user, index) => (
-				<SearchPerson key={index} user={user} />
-			))}
-		</>
-	);
+
+export default function Home({}) {
+  const [filterOption, setFilterOption] = useState({
+    OnlineFlag: false,
+    DistanceFlag: false,
+  });
+  const [users, setUsers] = useState([]);
+  const changeFormat = (data) => {
+    var listOfObjects = [];
+    data.forEach(function (entry) {
+      let found = false;
+      listOfObjects.forEach((eachObject) => {
+        if (eachObject.Username === entry.Username) {
+          found = true;
+          eachObject["Name"].push(entry["Name"]);
+        }
+      });
+      if (!found) {
+        var tmp = entry;
+        var tmpName = entry["Name"];
+        tmp["Name"] = [tmpName];
+        listOfObjects.push(tmp);
+      }
+    });
+    return listOfObjects;
+  };
+  useEffect(() => {
+    const fetching = async () => {
+      const response = await fetch(`/api/user`);
+      const x = await Promise.resolve(response.json());
+      setUsers(changeFormat(x));
+    };
+    fetching();
+  }, []);
+
+    const search = useCallback(
+      async (target) => {
+        try {
+          const response = await fetch(
+            `/api/user?SearchStr=${target}&OnlineFlag=${filterOption["OnlineFlag"]}&DistanceFlag=${filterOption["DistanceFlag"]}`,
+            {
+              method: "GET",
+            }
+          );
+          const x = await Promise.resolve(response.json());
+          setUsers(changeFormat(x[0]));
+        } catch {
+          alert("Something went wrong");
+        }
+      },
+      [filterOption]
+    );
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          search(e.target.searchbox.value);
+        }}
+      >
+        <input
+          className="textbox"
+          name="searchbox"
+          placeholder="ค้นหาแพทย์ โดย ชื่อ / อาการ / คลินิค"
+        />
+      </form>
+
+      <SearchTextWithFilter
+        text='All result for "สุชาติ"'
+        setFilter={setFilterOption}
+      />
+      {users.map((user, index) => (
+        <SearchPerson key={index} user={user} />
+      ))}
+    </>
+  );
 }
 
 Home.getLayout = function getLayout(page) {
-	return (
-		<Layout>
-			<Navbar />
-			<p className="text-blue-900 text-3xl font-bold text-center">
-				Search physicians
-			</p>
-			{page}
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Navbar />
+      <p className="text-blue-900 text-3xl font-bold text-center">
+        Search physicians
+      </p>
+      {page}
+    </Layout>
+  );
 };
