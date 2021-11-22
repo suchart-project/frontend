0faa65d5a37366b4473de9501364e920d762edd0
remove158 <remove158@gmail.com>
2021-11-22 16:46:16 +0700

    fix:  request filter status 0

--- src/pages/api/request.js
+++ src/pages/api/request.js
@@ -12,14 +12,14 @@ export default async function handler(req, res) {
 
 		if (Request_id && select) {
 			const [result] = await sqlConnection.execute(
-				"select * from REQUEST r, CUSTOMER c where  r.Request_id=? and c.Username = r.Patient_username",
+				"select * from REQUEST r, CUSTOMER c where  r.Status=0 and r.Request_id=? and c.Username = r.Patient_username",
 				[Request_id]
 			);
 			return res.status(200).json(result);
 		}
 		if (Request_id) {
 			const [result] = await sqlConnection.execute(
-				"select * from REQUEST r, CUSTOMER c where  r.Request_id=? and c.Username = r.Doctor_username",
+				"select * from REQUEST r, CUSTOMER c where r.Status=0 and  r.Request_id=? and c.Username = r.Doctor_username",
 				[Request_id]
 			);
 			return res.status(200).json(result);
@@ -27,20 +27,20 @@ export default async function handler(req, res) {
 
 		if (Patient_username) {
 			const [result] = await sqlConnection.execute(
-				"select * from REQUEST r, CUSTOMER c where  r.Patient_username=? and c.Username = r.Doctor_username",
+				"select * from REQUEST r, CUSTOMER c where r.Status=0 and r.Patient_username=? and c.Username = r.Doctor_username",
 				[Patient_username]
 			);
 			return res.status(200).json(result);
 		}
 		if (Doctor_username) {
 			const [result] = await sqlConnection.execute(
-				"select * from REQUEST r, CUSTOMER c where  r.Doctor_username=? and c.Username =  r.Patient_username",
+				"select * from REQUEST r, CUSTOMER c where r.Status=0 and r.Doctor_username=? and c.Username =  r.Patient_username",
 				[Doctor_username]
 			);
 			return res.status(200).json(result);
 		}
 		const [result] = await sqlConnection.execute(
-			"select * from REQUEST r, CUSTOMER c where c.Username = r.Patient_username"
+			"select * from REQUEST r, CUSTOMER c where r.Status=0 and c.Username = r.Patient_username"
 		);
 		return res.status(200).json(result);
 	}
@@ -61,17 +61,19 @@ export default async function handler(req, res) {
 	if (req.method === "PUT") {
 		// Usecase: physician accept or decline request
 		// TODO: <update> request and create Consultation if accepted
-		const { Status, Request_id } = JSON.parse(req.body);
+		console.log(req.body);
+		const { Status, Request_id } = req.body;
 		const [result] = await sqlConnection.execute(
 			"update REQUEST set Status=? where Request_id=?",
 			[Status, Request_id]
 		);
+		console.log(Status, Request_id);
 		if (Status === 1) {
 			const [result] = await sqlConnection.execute(
 				"select Patient_username,Doctor_username,Request_id from REQUEST where Request_id=?",
 				[Request_id]
 			);
-			db.collection("CONSULTATION").insertOne({
+			mongoConnection.collection("Consultation").insertOne({
 				physician: result[0].Doctor_username,
 				patient: result[0].Patient_username,
 				pay_amount: 0,
