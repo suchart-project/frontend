4c0563d56f822616edcbbabb7894f096998fa28a
remove158 <remove158@gmail.com>
2021-11-22 21:16:29 +0700

    ✅ add : search by text

--- src/components/SearchTextWithFilter.js
+++ src/components/SearchTextWithFilter.js
@@ -6,32 +6,28 @@ export default function SearchTextWithFilter({ text, setFilter }) {
 	const toggleMenu = () => setShowFilter(!showFilter);
 	const filterOption = ["Location", "Online"];
 	return (
-		text && (
-			<>
-				<div className="flex flex-row justify-between items-center px-4">
-					<a className="text-xl ">{text}</a>
-					<div className="icon relative group" onClick={toggleMenu}>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-5 w-5  text-gray-500"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-						>
-							<path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z" />
-						</svg>
+		<div className="flex flex-row justify-between items-center px-4">
+			<a className="text-xl ">{text || "ผลลัพธ์การค้นหาทั้งหมด"}</a>
+			<div className="icon relative group" onClick={toggleMenu}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					className="h-5 w-5  text-gray-500"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+				>
+					<path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z" />
+				</svg>
 
-						<div className="bg-white w-64  font-normal py-4 rounded-md absolute z-30 right-0 gap-2  flex flex-col px-4 transition-all duration-200 ease-in-out shadow-md scale-0 group-hover:scale-100">
-							{filterOption.map((title, index) => (
-								<FilterMenu
-									key={index}
-									setFilter={setFilter}
-									title={title}
-								/>
-							))}
-						</div>
-					</div>
+				<div className="bg-white w-64  font-normal py-4 rounded-md absolute z-30 right-0 gap-2  flex flex-col px-4 transition-all duration-200 ease-in-out shadow-md scale-0 group-hover:scale-100">
+					{filterOption.map((title, index) => (
+						<FilterMenu
+							key={index}
+							setFilter={setFilter}
+							title={title}
+						/>
+					))}
 				</div>
-			</>
-		)
+			</div>
+		</div>
 	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,93 +4,94 @@ import SearchPerson from "../components/SearchPerson";
 import SearchTextWithFilter from "../components/SearchTextWithFilter";
 import { useState, useCallback, useEffect } from "react";
 
-
 export default function Home({}) {
-  const [filterOption, setFilterOption] = useState({
-    OnlineFlag: false,
-    DistanceFlag: false,
-  });
-  const [users, setUsers] = useState([]);
-  const changeFormat = (data) => {
-    var listOfObjects = [];
-    data.forEach(function (entry) {
-      let found = false;
-      listOfObjects.forEach((eachObject) => {
-        if (eachObject.Username === entry.Username) {
-          found = true;
-          eachObject["Name"].push(entry["Name"]);
-        }
-      });
-      if (!found) {
-        var tmp = entry;
-        var tmpName = entry["Name"];
-        tmp["Name"] = [tmpName];
-        listOfObjects.push(tmp);
-      }
-    });
-    return listOfObjects;
-  };
-  useEffect(() => {
-    const fetching = async () => {
-      const response = await fetch(`/api/user`);
-      const x = await Promise.resolve(response.json());
-      setUsers(changeFormat(x));
-    };
-    fetching();
-  }, []);
+	const [filterOption, setFilterOption] = useState({
+		OnlineFlag: false,
+		DistanceFlag: false,
+	});
+	const [users, setUsers] = useState([]);
+	const [text, setText] = useState("");
+	const changeFormat = (data) => {
+		var listOfObjects = [];
+		data.forEach(function (entry) {
+			let found = false;
+			listOfObjects.forEach((eachObject) => {
+				if (eachObject.Username === entry.Username) {
+					found = true;
+					eachObject["Name"].push(entry["Name"]);
+				}
+			});
+			if (!found) {
+				var tmp = entry;
+				var tmpName = entry["Name"];
+				tmp["Name"] = [tmpName];
+				listOfObjects.push(tmp);
+			}
+		});
+		return listOfObjects;
+	};
+	useEffect(() => {
+		const fetching = async () => {
+			const response = await fetch(`/api/user`);
+			const x = await Promise.resolve(response.json());
+			setUsers(changeFormat(x));
+		};
+		fetching();
+	}, []);
 
-    const search = useCallback(
-      async (target) => {
-        try {
-          const response = await fetch(
-            `/api/user?SearchStr=${target}&OnlineFlag=${filterOption["OnlineFlag"]}&DistanceFlag=${filterOption["DistanceFlag"]}`,
-            {
-              method: "GET",
-            }
-          );
-          const x = await Promise.resolve(response.json());
-          setUsers(changeFormat(x[0]));
-        } catch {
-          alert("Something went wrong");
-        }
-      },
-      [filterOption]
-    );
+	const search = useCallback(
+		async (target) => {
+			try {
+				const response = await fetch(
+					`/api/user?SearchStr=${target}&OnlineFlag=${filterOption["OnlineFlag"]}&DistanceFlag=${filterOption["DistanceFlag"]}`,
+					{
+						method: "GET",
+					}
+				);
+				const x = await Promise.resolve(response.json());
+				setUsers(changeFormat(x[0]));
+			} catch {
+				alert("Something went wrong");
+			}
+		},
+		[filterOption]
+	);
 
-  return (
-    <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          search(e.target.searchbox.value);
-        }}
-      >
-        <input
-          className="textbox"
-          name="searchbox"
-          placeholder="ค้นหาแพทย์ โดย ชื่อ / อาการ / คลินิค"
-        />
-      </form>
+	return (
+		<>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					setText(e.target.searchbox.value);
+					search(e.target.searchbox.value);
+				}}
+			>
+				<input
+					className="textbox"
+					name="searchbox"
+					placeholder="ค้นหาแพทย์ โดย ชื่อ / อาการ / คลินิค"
+				/>
+			</form>
 
-      <SearchTextWithFilter
-        text='All result for "สุชาติ"'
-        setFilter={setFilterOption}
-      />
-      {users.map((user, index) => (
-        <SearchPerson key={index} user={user} />
-      ))}
-    </>
-  );
+			<SearchTextWithFilter
+				text={text ? `All result for "${text}"` : null}
+				setFilter={setFilterOption}
+			/>
+			{users.map((user, index) => (
+				<SearchPerson key={index} user={user} />
+			))}
+		</>
+	);
 }
 
 Home.getLayout = function getLayout(page) {
-  return (
-    <Layout>
-      <Navbar />
-      <p className="text-blue-900 text-3xl font-bold text-center">
-        Search physicians
-      </p>
-      {page}
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Navbar />
+			<p className="text-blue-900 text-3xl font-bold text-center">
+				Search physicians
+			</p>
+			{page}
+		</Layout>
+	);
 };
